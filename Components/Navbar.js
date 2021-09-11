5c071769694398fe0f207d0a608578bf54e6cbdb
Manikant Jha <mjontop0602@gmail.com>
2021-09-12 00:46:01 +0530

    Added Navbar UI

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import navbar from "../styles/Navbar.module.css";
 const Navbar = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div className={navbar.body}>
+      <header className={navbar.container}>
+        <div className={navbar.main}>
+          <div>
+            <i>HOME</i>
+          </div>
+          <div>
+            <input className={navbar.input} placeholder="Search" />
+          </div>
+          <div className="cursor-ptr">
+            <img
+              src="https://img-premium.flaticon.com/png/512/552/premium/552848.png?token=exp=1631388468~hmac=b3061e9fc90e9afde8381e20fc0901bd"
+              width="25"
+            />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Navbar from "../Components/Navbar";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+        crossorigin="anonymous"
+      />
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
