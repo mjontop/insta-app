324c435f2707284b334c5efed4cd707c73851059
Manikant Jha <mjontop0602@gmail.com>
2021-09-12 20:36:32 +0530

    Added Profile Page working

--- Components/Profile/Profile.js
+++ Components/Profile/Profile.js
@@ -1,11 +1,44 @@
-import React, { useEffect } from "react";
-import getUsersEmail from "./ProfileHelper";
+import React, { useEffect, useState } from "react";
+import getUsersEmail, { getUsersConnections } from "./ProfileHelper";
 
 const Profile = ({ username }) => {
+  const [userEmail, setUserEmail] = useState({ data: null, isLoading: false });
+  const [userConnections, setUserConnections] = useState({
+    followers: 0,
+    following: 0,
+    isLoading: false,
+  });
   useEffect(() => {
-    if (!!username) getUsersEmail(username);
+    if (!!username) {
+      setUserEmail({ ...userEmail, isLoading: true });
+      setUserConnections({ ...userConnections, isLoading: true });
+      getUsersEmail(username).then(({ error, ...data }) => {
+        if (!error) {
+          setUserEmail({ data, isLoading: false });
+          getUsersConnections(data.email).then((data) => {
+            if (!data.error) {
+              setUserConnections({
+                followers: data.followers,
+                following: data.following,
+                isLoading: false,
+              });
+            }
+          });
+          return;
+        }
+        setUserEmail((prev) => ({ ...prev, isLoading: false }));
+        setUserConnections((prev) => ({ ...prev, isLoading: false }));
+        return;
+      });
+    }
   }, [username]);
-  return <h1>Hey {username}</h1>;
+  return (
+    <>
+      <h1>Hey {username}</h1>
+      {JSON.stringify(userEmail)}
+      {JSON.stringify(userConnections)}
+    </>
+  );
 };
 
 export default Profile;
