3a04c455ec14479b0f916b294667e8365b046af3
Manikant Jha <mjontop0602@gmail.com>
2021-09-12 15:21:59 +0530

    Added routes in Nav

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,25 +1,33 @@
 import React from "react";
 import navbar from "../styles/Navbar.module.css";
-const Navbar = () => {
+import Link from "next/link";
+const Navbar = ({ children }) => {
   return (
-    <div className={navbar.body}>
-      <header className={navbar.container}>
-        <div className={navbar.main}>
-          <div>
-            <i>HOME</i>
-          </div>
-          <div>
-            <input className={navbar.input} placeholder="Search" />
-          </div>
-          <div className="cursor-ptr">
-            <img
-              src="https://img-premium.flaticon.com/png/512/552/premium/552848.png?token=exp=1631388468~hmac=b3061e9fc90e9afde8381e20fc0901bd"
-              width="25"
-            />
+    <>
+      <header className={navbar.body}>
+        <div className={navbar.container}>
+          <div className={navbar.main}>
+            <Link href="/">
+              <div className="cursor-ptr">
+                <i>HOME</i>
+              </div>
+            </Link>
+            <div>
+              <input className={navbar.input} placeholder="Search" />
+            </div>
+            <Link href="/profile">
+              <div className="cursor-ptr">
+                <img
+                  src="https://img-premium.flaticon.com/png/512/552/premium/552848.png?token=exp=1631388468~hmac=b3061e9fc90e9afde8381e20fc0901bd"
+                  width="25"
+                />
+              </div>
+            </Link>
           </div>
         </div>
       </header>
-    </div>
+      {children}
+    </>
   );
 };
 
