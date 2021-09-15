812529cb522d59c850259bffe89f6069843b5fbf
Manikant Jha <mjontop0602@gmail.com>
2021-09-15 23:15:30 +0530

    Completed profile UI Top-half

--- Components/Profile/Profile.js
+++ Components/Profile/Profile.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import SettingsOutlinedIcon from "@material-ui/icons/SettingsOutlined";
+
 import getUsersEmail, { getUsersConnections } from "./ProfileHelper";
 import style from "../../styles/Profile.module.css";
 
@@ -46,8 +48,34 @@ const Profile = ({ username }) => {
             />
           </div>
         </div>
-        <div className={style.userStats}></div>
+        <div className={style.userStats}>
+          <div className={style.user_name_row}>
+            <div className="px-2 fs-3">
+              <strong>{username}</strong>
+            </div>
+            <div className="px-2">
+              <button className="btn btn-secondary py-0">Follow</button>
+            </div>
+            <div className="px-2 cursor-ptr">
+              <SettingsOutlinedIcon />
+            </div>
+          </div>
+          <div className={style.stats_row}>
+            <div className="px-2">0 posts</div>
+            <div className="px-4 cursor-ptr">1 followers</div>
+            <div className="px-4 cursor-ptr">1 following</div>
+          </div>
+          <div className={style.name_bio_row}>
+            <div className="px-2">
+              <p>Name</p>
+            </div>
+            <div className="px-2">
+              <p>Bio</p>
+            </div>
+          </div>
+        </div>
       </div>
+      <hr />
       <div className={style.posts}>dfasdfasdfa</div>
     </main>
   );
