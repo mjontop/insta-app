60d88b3ab29162bcf696ab5b3803e7fcb8b20fe3
Manikant Jha <mjontop0602@gmail.com>
2021-09-26 23:58:52 +0530

    Added token in apis

--- utils/Axios.js
+++ utils/Axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
+const ISSERVER = typeof window === "undefined";
 
 const Axios = axios.create({
   baseURL: "http://localhost:8001/api",
-  // headers: { "X-Custom-Header": "foobar" },
+  headers: {
+    "x-auth-token": !ISSERVER ? localStorage.getItem("token") : "",
+  },
 });
 
 export default Axios;
