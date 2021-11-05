3cb46960668c30a4f87800fad34ee2c930df74c4
Manikant Jha <mjontop0602@gmail.com>
2021-11-05 23:25:06 +0530

    Added Create new post UI complete

--- Components/Post/CreatePost.js
+++ Components/Post/CreatePost.js
@@ -4,21 +4,27 @@ import { Button, Input } from "@material-ui/core";
 import style from "../../styles/CreatePost.module.css";
 const CreatePost = () => {
   const [imageUploaded, setImgUploaded] = useState(null);
-  const handleChange = (e) => {
+  const [caption, setCaption] = useState("");
+  const handleImageUpload = (e) => {
     console.log(Array.from(e.target.files));
     setImgUploaded(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handleCaptionChange = (e) => {
+    setCaption(e.target.value);
   };
 
   return (
     <div className="main">
       <div className={style.body}>
         <div className={style.heading}>
-          <strong className="text-center w-100 fs-1">Create New Post</strong>
+          <strong className="text-center w-100 fs-1">Create new post</strong>
+          <hr />
         </div>
         <div className={style.dragdrop}>
           {imageUploaded && (
             <div className={style.image}>
-              <img src={imageUploaded} width="400" height="500" />
+              <img className={style.img_uploaded} src={imageUploaded} />
               <ClearRounded
                 style={{ color: "red", background: "lightsalmon" }}
                 className="cursor-ptr"
@@ -33,15 +39,48 @@ const CreatePost = () => {
                 id="contained-button-file"
                 type="file"
                 className="d-none"
-                onChange={handleChange}
+                onChange={handleImageUpload}
               />
-              <Button variant="contained" component="span" className="btn" size="large">
-                Upload <BackupRounded />
+              <Button
+                variant="contained"
+                component="span"
+                className="btn"
+                size="large"
+              >
+                Upload From Device <BackupRounded />
               </Button>
             </label>
           )}
         </div>
-        <div className={style.footer}></div>
+        {!!imageUploaded && (
+          <div className={style.footer}>
+            <div className={style.userImage}>
+              <img
+                src="https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                height="150"
+                width="150"
+              />
+            </div>
+            <div className={style.captions}>
+              <textarea
+                placeholder="Write Something about Photo!"
+                maxLength="120"
+                className={style.captionText}
+                value={caption}
+                onChange={handleCaptionChange}
+              />
+              <span>{caption.length}/120</span>
+              <Button
+                variant="contained"
+                component="span"
+                className="btn"
+                size="large"
+              >
+                Share
+              </Button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
